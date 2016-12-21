91bd121686a1fa00cd2234f44ce6a1469f97a502
vladutbutnaru <vlad.butnaru@feel-it-services.com>
2016-12-21 18:27:51 +0200

    Added blog servlet

---
+++ src/main/java/ro/enered/api/Blog.java
@@ -0,0 +1,20 @@
+package ro.enered.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by macbook on 21/12/2016.
+ */
+public class Blog extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
